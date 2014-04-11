d9452eacf55ad8ac4ec323243d12e40ea6835f25
James Treanor <jtreanor3@gmail.com>
2014-04-11 11:44:02 +0100

    Started answer for functional 2

--- CS4621/Summer2013.hs
+++ CS4621/Summer2013.hs
@@ -3,19 +3,12 @@
 --(a)
 data List a = EmptyList | L a (List a) deriving Show
 
-fromList :: [a] -> List a
-fromList []     = EmptyList
-fromList (x:xs) = L x (fromList xs) 
-
-(#:) :: a -> List a -> List a
-(#:) x xs = L x xs
-
 --(b)
 count :: (Eq a) => a -> List a -> Int
 count _ EmptyList = 0
 count x (L y xs)
-	| y == x    = 1 + count x xs
-	| otherwise = count x xs
+    | y == x    = 1 + count x xs
+    | otherwise = count x xs
 
 append :: List a -> List a -> List a
 append EmptyList ys = ys
@@ -23,6 +16,19 @@ append (L x xs) ys  = L x (append xs ys)
 
 --Q2
 
+--Not done yet
+
 --Q3
 
 --(a)
+
+-- z takes an integer s1 (in this case it is 3) and sums the top s1 elements on
+-- the stack, the result will be on top of the stack.
+-- After the example is run, the stack is as follows:
+--                                                    18
+--                                                    4
+--                                                    3
+--                                                    2
+--                                                    1
+
+--(b) Not done yet
