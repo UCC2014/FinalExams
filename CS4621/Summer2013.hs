b1680e0f3f8feea0245cf642e49d317b036aa9ec
James Treanor <jtreanor3@gmail.com>
2014-04-24 16:47:57 +0100

    Finished 2013 Summer.

--- CS4621/Summer2013.hs
+++ CS4621/Summer2013.hs
@@ -45,11 +45,42 @@ reach' g search seen ( n : ns )
 
 -- z takes an integer s1 (in this case it is 3) and sums the top s1 elements on
 -- the stack, the result will be on top of the stack.
--- After the example is run, the stack is as follows:
+--
+-- After "1 2 3 4 5 6 7 3 z ." is run, the stack is as follows (5 + 6 + 7 = 18):
 --                                                    18
 --                                                    4
 --                                                    3
 --                                                    2
 --                                                    1
 
---(b) Not done yet
+--(b) I have tested the following and it works.
+
+-- 1. Define a function in the factor interpretor called eval'repeat, as follows:
+
+    --eval'repeat :: [ Token ] -> Stack -> Environment -> String
+    --eval'repeat ts ( Sinteger n : Squotation q : ss ) env =
+    --        eval' ( ( repeatList q n ) ++ ts ) ss env
+
+    ---- repeatList xs n : the list containing the contents of list 'xs' 'n' times
+    --repeatList :: [ a ] -> Integer -> [ a ]
+    --repeatList xs 0 = [ ]
+    --repeatList xs n = xs ++ ( repeatList xs ( n - 1 ) )
+
+-- 2. Change eval' to be defined as follows:
+
+-- eval' tokens stack env : the result of interpreting the token list 'tokens'
+--                          using the stack 'stack' and the environment 'env'
+
+    --eval' :: [ Token ] -> Stack -> Environment -> String
+
+    --eval' [ ]        _     _   = ""
+
+    --eval' ( t : ts ) stack env = ...
+    --                             if t == "if" then
+    --                                ...
+    --                             else
+    --                             if t == "repeat" then
+    --                                eval'repeat ts stack env
+    --                             else
+    --                             case t of
+    --                                ...
